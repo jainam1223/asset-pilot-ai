f7a0b9a3656268fd5e4ced41ab8bc7e70ee012d3
Sakshee <sakshee.zala@simformsolutions.com>
2026-07-05 05:48:58 +0530

    Fix App Service configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,8 +20,10 @@ resource "azurerm_linux_web_app" "app" {
 
   site_config {
     application_stack {
-      docker_image_name   = "asset-pilot-ai:${var.docker_image_tag}"
-      docker_registry_url = "https://${data.azurerm_container_registry.acr.login_server}"
+      docker_image_name        = "asset-pilot-ai:${var.docker_image_tag}"
+      docker_registry_url      = "https://${data.azurerm_container_registry.acr.login_server}"
+      docker_registry_username = data.azurerm_container_registry.acr.admin_username
+      docker_registry_password = data.azurerm_container_registry.acr.admin_password
     }
   }
 
@@ -37,5 +39,6 @@ resource "azurerm_linux_web_app" "app" {
     "DOCKER_REGISTRY_SERVER_USERNAME"     = data.azurerm_container_registry.acr.admin_username
     "DOCKER_REGISTRY_SERVER_PASSWORD"     = data.azurerm_container_registry.acr.admin_password
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "WEBSITES_PORT"                       = "8000"
   }
 }
